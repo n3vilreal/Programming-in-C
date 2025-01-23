5c78d073758976c00bd80ed2dc8de3b9346ca11d
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-23 15:33:54 +0545

    program add

---
+++ String/concatinate-without-header.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    char s1[25],s2[25];
+    int i,j=0,l;
+    printf("\nEnter first string: ");
+    scanf("%s",s1);
+    printf("\nEnter second string: ");
+    scanf("%s",s2);
+    for(i=0;s1[i]!='\0';i++)
+    {
+        l++;
+    }
+    for(i=l;s2[j]!='\0';i++)
+    {
+        s1[i]=s2[j];
+        j++;
+    }
+    printf("\nThe concatinated string is: %s",s1);
+    return 0;
+}
