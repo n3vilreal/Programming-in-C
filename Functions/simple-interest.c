a53d6b05367e4c0b9df61e39aeb1ec725d0e396e
n3vilreal <nebil.241423@ncit.edu.np>
2025-01-24 12:48:14 +0545

    si func

---
+++ Functions/simple-interest.c
@@ -0,0 +1,25 @@
+//Display Simple Interest using function
+//Given double simpleInterest(double Principle,float rate, int time);
+
+#include<stdio.h>
+
+double simpleInterest(double Principle,float rate, int time);
+
+double Principle;
+float rate;
+int time;
+
+int main()
+{   
+    double si;
+    printf("\nEnter Principle, Rate and Time: \n");
+    scanf("%lf %f %d",&Principle, &rate, &time);
+    si = simpleInterest(Principle,rate,time);
+    printf("\nSimple interest: %0.2lf", si);
+    return 0;
+}
+
+double simpleInterest(double Principle,float rate, int time)
+{
+    return(Principle*rate*time*0.01);
+}
